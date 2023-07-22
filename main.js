8aaa9f34f559219baf2a9f957f3cd639f8eb4781
Luka-DV <luka.guva@gmail.com>
2023-07-22 18:56:27 +0200

    added new katas

--- main.js
+++ main.js
@@ -1656,8 +1656,10 @@ function noSpace(x){return x.split(' ').join('')}
 
 //join doesnt include the empty strings produced by split!
 
-
 console.log(noSpace(" aa aa b   ss ss  "))
+
+
+//+++++ Create a function that takes a string and returns an array of character that are unique in order ("AACCCS" => [A, C, S])
 
 
 const  uniqueInOrder=function(iterable){
@@ -1677,3 +1679,27 @@ const  uniqueInOrder=function(iterable){
 
 
 console.log(uniqueInOrder('AAAABBBCCDAABBB'));
+
+
+/* +++++++7 kyu
+You're a square!
+However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+Task
+
+Given an integral number, determine if it's a square number:
+
+    In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+
+The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+Examples
+
+-1  =>  false
+ 0  =>  true
+ 3  =>  false
+ 4  =>  true
+25  =>  true
+26  =>  false */
+
+const isSquare = function(n){
+  return Math.sqrt(n) % 1 === 0 ? true : false;
+};
