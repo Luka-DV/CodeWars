cc4e2911dbafe6dbcd880b642f9f4490c99a80ec
Luka-DV <luka.guva@gmail.com>
2023-07-23 15:14:38 +0200

    added solved katas

--- main.js
+++ main.js
@@ -1771,3 +1771,58 @@ function XO(str) {
 
   return numOfOs === numOfXs;
 }
+
+
+/* 6 kyu
+Multiples of 3 or 5
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once.
+
+Courtesy of projecteuler.net (Problem 1) */
+
+function solution(number){
+  if(number <= 2) return 0;
+
+  const multiplesArray = [];
+  for(let i = 3; i < number; i++) {
+    if( i % 3 === 0) {
+      multiplesArray.push(i);
+    } else if (i % 5 === 0) {
+      multiplesArray.push(i)
+    };
+  };
+  return multiplesArray.reduce((acc, crr) => acc + crr, 0);
+};
+
+//simpler:
+
+function solution(number){
+  if(number <= 2) return 0;
+
+  let numSolution = 0;
+  for(let i = 3; i < number; i++) {
+    if( i % 3 === 0) {
+      numSolution += i;
+    } else if (i % 5 === 0) {
+      numSolution += i;
+    };
+  };
+  return numSolution;
+};
+
+//or just:
+
+function solution(number){
+  if(number <= 2) return 0;
+
+  let numSolution = 0;
+  for(let i = 3; i < number; i++) {
+    if( i % 3 === 0 || i % 5 === 0) {
+      numSolution += i;
+    };
+  };
+  return numSolution;
+};
