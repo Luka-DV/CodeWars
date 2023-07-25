735b89e367764eb8bfe6ac3f960fcfb3716550b0
Luka-DV <luka.guva@gmail.com>
2023-07-25 11:20:25 +0200

    added new kata

--- main.js
+++ main.js
@@ -1846,3 +1846,24 @@ function abbrevName(name){
   return `${nameArray[0][0].toUpperCase()}.${nameArray[1][0].toUpperCase()}`;
 
 }
+
+/* 8 kyu
+Return Negative
+In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+Examples
+
+makeNegative(1);    // return -1
+makeNegative(-5);   // return -5
+makeNegative(0);    // return 0
+makeNegative(0.12); // return -0.12
+
+Notes
+
+    The number can be negative already, in which case no change is required.
+    Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense. */
+
+
+function makeNegative(num) {
+  return num <= 0 ? num : -num;
+}
+
