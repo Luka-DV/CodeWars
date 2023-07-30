cb63f89d2fdaecf006073bc8bd24f6f9d6b38ad2
Luka-DV <luka.guva@gmail.com>
2023-07-30 10:16:12 +0200

    added Break camelCase kata

--- main.js
+++ main.js
@@ -2054,3 +2054,22 @@ Notes:
       let idx = money.indexOf(Math.max(...money));
       return students[idx].name;
     }
+
+
+
+/*     6 kyu
+Break camelCase
+Complete the solution so that the function will break up camel casing, using a space between words.
+Example
+
+"camelCasing"  =>  "camel Casing"
+"identifier"   =>  "identifier"
+""             =>  ""
+
+ */
+
+function solution(string) {
+  return string.split("")
+  .map(e => e === e.toUpperCase() ? ` ${e}`: e)
+  .join("")
+}
