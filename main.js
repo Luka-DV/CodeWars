cf0f383f8b2f40411121b3e4d3be74dd5f493604
Luka-DV <luka.guva@gmail.com>
2023-07-31 09:04:12 +0200

    added kata Needle in Haystack

--- main.js
+++ main.js
@@ -2073,3 +2073,24 @@ function solution(string) {
   .map(e => e === e.toUpperCase() ? ` ${e}`: e)
   .join("")
 }
+
+/* 8 kyu
+A Needle in the Haystack
+Can you find the needle in the haystack?
+
+Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+After your function finds the needle it should return a message (as a string) that says:
+
+"found the needle at position " plus the index it found the needle, so:
+
+Example(Input --> Output)
+
+["hay", "junk", "hay", "hay", "moreJunk", "needle", "randomJunk"] --> "found the needle at position 5" 
+
+Note: In COBOL, it should return "found the needle at position 6" */
+
+function findNeedle(haystack) {
+  const needlePosition = haystack.indexOf("needle");
+  return `found the needle at position ${needlePosition}`;
+}
