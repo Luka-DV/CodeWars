f81cf2cf97aa155ab534dd82b75c96e0dff6ae89
Luka-DV <luka.guva@gmail.com>
2023-08-18 18:07:12 +0200

    added new kata

--- main.js
+++ main.js
@@ -2396,3 +2396,70 @@ function Ship(draft,crew) {
  Ship.prototype.isWorthIt = function() {
    return this.draft - (this.crew*1.5) > 20 ? true : false;
  }
+
+/* +++++++++++++++++++++ 7 kyu
+Selective fear of numbers
+I've got a crazy mental illness. I dislike numbers a lot. But it's a little complicated: The number I'm afraid of depends on which day of the week it is... This is a concrete description of my mental illness:
+
+Monday --> 12
+
+Tuesday --> numbers greater than 95
+
+Wednesday --> 34
+
+Thursday --> 0
+
+Friday --> numbers divisible by 2
+
+Saturday --> 56
+
+Sunday --> 666 or -666
+
+Write a function which takes a string (day of the week) and an integer (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean) */
+
+
+//You could write normal if...else statements, but this is a more elegant solution (using an object):
+
+function AmIAfraid(day, num) {
+  
+  const dayNumberCondition = {
+    "Monday": num === 12,
+    "Tuesday": num > 95,
+    "Wednesday": num === 34,
+    "Thursday": num === 0,
+    "Friday": num % 2 === 0,
+    "Saturday": num === 56,
+    "Sunday": num === 666 || num === -666
+  }
+  
+  return dayNumberCondition[day];
+}
+
+//other CW solutions:
+
+//1:
+
+var AmIAfraid = function(day, num) {
+  var preds = {"Sunday":    d => d == 666 || d == -666,
+               "Monday":    d => d == 12,
+               "Tuesday":   d => d > 95,
+               "Wednesday": d => d == 34,
+               "Thursday":  d => d === 0,
+               "Friday":    d => d % 2 === 0,
+               "Saturday":  d => d == 56};
+  return preds[day](num);
+}
+
+//2:
+
+function AmIAfraid(day, num) {
+  return {
+    "Monday"   : num === 12,
+    "Tuesday"  : num > 95,
+    "Wednesday": num === 34,
+    "Thursday" : num === 0,
+    "Friday"   : num % 2 === 0,
+    "Saturday" : num === 56,
+    "Sunday"   : Math.abs(num) === 666
+  }[day];
+}
