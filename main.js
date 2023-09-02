2d1196128d430dd55ade11b0d580e566404dcc84
Luka-DV <luka.guva@gmail.com>
2023-09-02 11:53:15 +0200

    add  kata (7OOP)

--- main.js
+++ main.js
@@ -3021,3 +3021,94 @@ class Quark{
 const quark1 = new Quark("green", "up");
 const quark2 = new Quark("blue", "down")
 console.log(quark1, quark2)
+
+
+/* 7 kyu
+Building blocks
+Write a class Block that creates a block (Duh..)
+Requirements
+
+The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+
+Define these methods:
+
+`getWidth()` return the width of the `Block`
+
+`getLength()` return the length of the `Block`
+
+`getHeight()` return the height of the `Block`
+
+`getVolume()` return the volume of the `Block`
+
+`getSurfaceArea()` return the surface area of the `Block`
+
+Examples
+
+    let b = new Block([2,4,6]) -> creates a `Block` object with a width of `2` a length of `4` and a height of `6`
+    b.getWidth() // -> 2
+    
+    b.getLength() // -> 4
+    
+    b.getHeight() // -> 6
+    
+    b.getVolume() // -> 48
+    
+    b.getSurfaceArea() // -> 88
+
+Note: no error checking is needed */ž
+
+
+class Block{
+
+  constructor([width, length, height]){
+    this.width = width,
+    this.length = length,
+    this.height = height
+  };
+
+  getWidth() {
+    return this.width
+  }
+  getLength() {
+    return this.length
+  }
+  getHeight() {
+    return this.height
+  }
+  getVolume() {
+    return this.height*this.length*this.width
+  }
+  getSurfaceArea() {
+    return 2*this.height*this.width + 2*this.height*this.length + 2*this.width*this.length
+  }
+  
+}
+
+//more elegant:
+
+class Block{
+
+  constructor(data){
+    [this.width,this.length,this.height] = data;
+  };
+
+  getWidth() {
+    return this.width
+  }
+  getLength() {
+    return this.length
+  }
+  getHeight() {
+    return this.height
+  }
+  getVolume() {
+    const {height, length, width} = this
+    return height*length*width
+  }
+  getSurfaceArea() {
+    const {height, length, width} = this
+    return 2*height*width + 2*height*length + 2*width*length
+  }
+  
+}
+
