2b6a921d8239c12022686af9b5df16b281fd060b
Luka-DV <luka.guva@gmail.com>
2023-09-05 11:01:37 +0200

    add  kata (7)

--- main.js
+++ main.js
@@ -3131,3 +3131,70 @@ All values in the array will be integers. Return the good values in the order th
 function noOdds( values ){
   return values.filter(num => num % 2 === 0);
 }
+
+/* 7 kyu
+Check the exam
+The first input array is the key to the correct answers to an exam, like ["a", "a", "b", "d"]. The second one contains a student's submitted answers.
+
+The two arrays are not empty and are the same length. Return the score for this array of answers, giving +4 for each correct answer, -1 for each incorrect answer, and +0 for each blank answer, represented as an empty string (in C the space character is used).
+
+If the score < 0, return 0.
+
+For example:
+
+checkExam(["a", "a", "b", "b"], ["a", "c", "b", "d"]) → 6
+checkExam(["a", "a", "c", "b"], ["a", "a", "b",  ""]) → 7
+checkExam(["a", "a", "b", "c"], ["a", "a", "b", "c"]) → 16
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) → 0 */
+
+
+
+function checkExam(array1, array2) {
+  let score = 0;
+ for(let i = 0; i < array1.length; i++) {
+   if(!array2[i]) score += 0;
+   else if(array1[i] === array2[i]) score += 4;
+   else score -= 1;
+ }
+  if(score < 0) return 0;
+  return score;
+}
+
+//using reduce:
+
+const checkExam = (array1, array2) => {
+  let score = array2.reduce(
+    (acc, crr, i) => {
+      if(crr == array1[i]) return acc += 4;
+      else if(crr == 0) return acc += 0;
+      else return acc - 1;
+    }
+  , 0);
+  return score < 0 ? 0 : score;
+}
+
+//solution with a Map. Not space efficient (O(n) vs O(1))
+
+function checkExam(array1, array2) {
+
+  let score = 0;
+  
+  for(let i = 0; i < array1.length; i++) {
+
+    const checkAnswer = new Map([
+      [array1[i] === array2[i], 4],
+      [array1[i] !== array2[i], -1],
+      [!array2[i], 0]
+    ])
+  
+  
+    for(let [key, value] of checkAnswer) {
+      if(key) score += value
+    }
+  }
+  
+  if(score < 0) return 0;
+  return score
+}
+
+
