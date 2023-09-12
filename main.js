8f0d238787284c28f30bcb8fdba5ba73c7b705c1
Luka-DV <luka.guva@gmail.com>
2023-09-12 09:02:25 +0200

    add 'Make a function that does arithmetic' kata (7)

--- main.js
+++ main.js
@@ -3478,3 +3478,45 @@ function stray(numbers) {
 }
 
 console.log(stray([2,2,2,2,4]))
+
+
+/* 7 kyu
+Make a function that does arithmetic!
+Given two numbers and an arithmetic operator (the name of it, as a string), return the result of the two numbers having that operator used on them.
+
+a and b will both be positive integers, and a will always be the first number in the operation, and b always the second.
+
+The four operators are "add", "subtract", "divide", "multiply".
+
+A few examples:(Input1, Input2, Input3 --> Output) */
+
+function arithmetic(a, b, operator){
+  const operatorObject = {
+    add: (a,b) => a + b,
+    subtract: (a,b) => a - b,
+    multiply: (a,b) => a * b,
+    divide: (a,b) => a / b
+  };  
+  return operatorObject[operator](a,b)
+}
+
+//or:
+
+const arithmetic = (a, b, operator) => ({
+  'add'     : a + b,
+  'subtract': a - b,
+  'multiply': a * b,
+  'divide'  : a / b
+}[operator]);
+
+//or: 
+
+function arithmetic(a, b, operator){
+  const operatorObject = {
+    add: a + b,
+    subtract: a - b,
+    multiply: a * b,
+    divide: a / b
+  };  
+  return operatorObject[operator]
+} 
