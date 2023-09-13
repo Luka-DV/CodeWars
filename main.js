5758e547d6bf10d459da4021bcd835d5ad3f04bb
Luka-DV <luka.guva@gmail.com>
2023-09-13 10:35:29 +0200

    add  kata (7)

--- main.js
+++ main.js
@@ -3520,3 +3520,28 @@ function arithmetic(a, b, operator){
   };  
   return operatorObject[operator]
 } 
+
+/* +++++7 kyu
+Length and two values.
+
+Given an integer n and two other values, build an array of size n filled with these two values alternating. */
+
+function alternate(n, firstValue, secondValue){
+  const arrayWithValues = [];
+  for(let i = 1; i <= n; i++){
+    if(i % 2 === 0) {
+      arrayWithValues.push(secondValue)
+    } else arrayWithValues.push(firstValue);
+  }
+  return arrayWithValues
+  }
+
+  //or:
+
+  function alternate(n, firstValue, secondValue){
+    const array = [];
+    for (let i = 0; i < n; i++) {
+      array.push(i % 2 === 0 ? firstValue : secondValue)
+    }
+    return array;
+  }
