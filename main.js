712d20e79fea7d4b6d3c9830f866e995805ed4cb
Luka-DV <luka.guva@gmail.com>
2023-09-26 19:19:55 +0200

    add Sum of a sequence kata (7)

--- main.js
+++ main.js
@@ -3931,7 +3931,7 @@ function deleteNth2(arr,n){
 }
 
 
-/* 8 kyu
+/* ++++++++++8 kyu
 Grasshopper - Check for factor
 This function should test if the factor is a factor of base.
 
@@ -3939,5 +3939,26 @@ Return true if it is a factor or false if it is not. */
 
 function checkForFactor (base, factor) {
   return base % factor === 0;
+}
+
+
+/* +++++++++7 kyu
+Sum of a sequence
+Your task is to write a function which returns the sum of a sequence of integers.
+The sequence is defined by 3 non-negative values: begin, end, step.
+If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+Examples
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4) */
+
+const sequenceSum = (begin, end, step) => {
+  let result = 0;
+  for(let i = begin; i <= end; i += step) {
+    result += i;
+  }
+  return result;
 }
 
