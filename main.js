dfe9b7f4ad6d651983c476fec49c52d9ae61503d
Luka-DV <luka.guva@gmail.com>
2023-10-07 11:45:31 +0200

    add kata ''

--- main.js
+++ main.js
@@ -4234,6 +4234,35 @@ console.log(helper.pageIndex(1), "shoudl return 0") */
 
 
 
+/* +++++++++++7 kyu
+Two fighters, one winner.
+
+Create a function that returns the name of the winner in a fight between two fighters.
+
+Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+Example:
+
+  declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+  
+  Lew attacks Harry; Harry now has 3 health.
+  Harry attacks Lew; Lew now has 6 health.
+  Lew attacks Harry; Harry now has 1 health.
+  Harry attacks Lew; Lew now has 2 health.
+  Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+
+function Fighter(name, health, damagePerAttack) {
+        this.name = name;
+        this.health = health;
+        this.damagePerAttack = damagePerAttack;
+        this.toString = function() { return this.name; }
+}
+ */
 
 
 //first version:
@@ -4291,8 +4320,9 @@ function declareWinner2(fighter1, fighter2, firstAttacker) {
   }
 }
 
-//smart CW solution:
+//smart CW solutions:
 
+// 1.
 function declareWinner0(fighter1, fighter2, firstAttacker) {
   var fac1 = Math.ceil( fighter1.health / fighter2.damagePerAttack );
   var fac2 = Math.ceil( fighter2.health / fighter1.damagePerAttack );
@@ -4304,3 +4334,42 @@ function declareWinner0(fighter1, fighter2, firstAttacker) {
     return firstAttacker;
   }
 }
+
+// 2.
+
+function declareWinner00(fighter1, fighter2, firstAttacker) {
+  while (fighter1.health > 0 && fighter2.health > 0) {
+    fighter2.health -= fighter1.damagePerAttack;
+    fighter1.health -= fighter2.damagePerAttack;
+  }
+  
+  if (fighter1.health <= 0 && fighter2.health <= 0)
+    return firstAttacker;
+  else if (fighter1.health <= 0)
+    return fighter2.name;
+  else
+    return fighter1.name;
+}
+
+
+
+/* 8 kyu
+Reversing Words in a String
+
+You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+Example (Input --> Output)
+
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi" */
+
+
+function reverse99(string){
+  let withoutSpaces = string.trim();
+  return withoutSpaces.split(" ")
+        .reverse()
+        .join(" ");
+}
+
