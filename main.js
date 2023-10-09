89fe0adff7f15f2d8a338fc144d0fe2cb60d52b3
Luka-DV <luka.guva@gmail.com>
2023-10-09 09:28:35 +0200

    add kata 'Sum of Minimums' (7)

--- main.js
+++ main.js
@@ -4376,7 +4376,7 @@ function reverse99(string){
 
 
 /* ++++++++++++8 kyu
-Replace all vowel to exclamation mark in the sentence */
+Replace all vowels to exclamation mark in the sentence */
 
 //using regex:
 
@@ -4396,3 +4396,28 @@ function replace(s){
   } )
   .join("");
 }
+
+
+/* 7 kyu
+Sum of Minimums!
+Given a 2D ( nested ) list ( array, vector, .. ) of size m * n, your task is to find the sum of the minimum values in each row.
+
+For Example:
+
+[ [ 1, 2, 3, 4, 5 ]        #  minimum value of row is 1
+, [ 5, 6, 7, 8, 9 ]        #  minimum value of row is 5
+, [ 20, 21, 34, 56, 100 ]  #  minimum value of row is 20
+]
+
+So the function should return 26 because the sum of the minimums is 1 + 5 + 20 = 26.
+
+Note: You will always be given a non-empty list containing positive values. */
+
+function sumOfMinimums(arr) {
+  return arr.reduce((acc, crr) => {
+    return acc + Math.min(...crr);
+  },0)
+};
+
+//the fastest way to find the max value of an array is actually using a normal for loop.
+
