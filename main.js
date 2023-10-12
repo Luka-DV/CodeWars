8aa7af5718071688304417f9b831d919fb252405
Luka-DV <luka.guva@gmail.com>
2023-10-12 10:36:02 +0200

    Coding Meetup 2 Higher-OrderFunctions Series Greet developers

--- main.js
+++ main.js
@@ -4478,3 +4478,28 @@ function countDevelopers2(list) {
 
 
 
+/* 7 kyu
+Coding Meetup #2 - Higher-Order Functions Series - Greet developers
+
+You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+
+Hi < firstName here >, what do you like the most about < language here >? */
+
+function greetDevelopers1(list) {
+  return list.map(person => {
+    person.greeting = `Hi ${person.firstName}, what do you like the most about ${person.language}?`;
+    return person;
+  })
+}
+
+//OR:
+
+function greetDevelopers2(list) {
+  list.forEach(function(developer) {
+    developer.greeting = `Hi ${developer.firstName}, what do you like the most about ${developer.language}?`;
+  });
+  
+  return list;
+}
