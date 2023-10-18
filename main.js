fd799b41dcddd8805fd782b64eb7ebac114689e4
Luka-DV <luka.guva@gmail.com>
2023-10-18 19:24:18 +0200

    add coding meetup 7 kata (6)

--- main.js
+++ main.js
@@ -4571,7 +4571,7 @@ false otherwise. */
   }
 
 
-/* 6 kyu
+/* +++++++6 kyu
 Sort My Animals
 
 Consider the following class:
@@ -4603,4 +4603,31 @@ const compareAnimals02 = (a, b) =>
   a.numberOfLegs - b.numberOfLegs || a.name.localeCompare(b.name)
   
 const sortAnimal = animals =>
-  animals ? animals.slice().sort(compareAnimals02) : null
+  animals ? animals.slice().sort(compareAnimals02) : nullgit
+
+
+/*   +++++++++++6 kyu
+Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer
+You will be given a sequence of objects representing data about developers who have signed up to attend the next coding meetup that you are organising.
+Your task is to return a sequence which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+For example, given the following input array:
+var list1 = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+your function should return the following array:
+[
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
+Notes:
+    The input array will always be valid and formatted as in the example above and will never be empty. */
+
+    
+
+function findSenior(list) {
+
+  return list.filter(developer => developer.age === Math.max(...list.map(dev => dev.age)));
+}
