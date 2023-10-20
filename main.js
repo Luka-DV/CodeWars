f89d78309c4f932ef06751a33392841ce03bf078
Luka-DV <luka.guva@gmail.com>
2023-10-20 12:05:21 +0200

    add Coding Meetup 8 - All continents kata (6)

--- main.js
+++ main.js
@@ -4625,9 +4625,49 @@ your function should return the following array:
 Notes:
     The input array will always be valid and formatted as in the example above and will never be empty. */
 
-    
+
 
 function findSenior(list) {
 
   return list.filter(developer => developer.age === Math.max(...list.map(dev => dev.age)));
+}
+
+
+
+/* 6 kyu
+Coding Meetup #8 - Higher-Order Functions Series - Will all continents be represented?
+You will be given a sequence of objects (associative arrays in PHP) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+
+Your task is to return:
+
+    true if all of the following continents / geographic zones will be represented by at least one developer: 'Africa', 'Americas', 'Asia', 'Europe', 'Oceania'.
+    false otherwise.
+
+For example, given the following input array:
+var list1 = [
+  { firstName: 'Fatima', lastName: 'A.', country: 'Algeria', continent: 'Africa', age: 25, language: 'JavaScript' },
+  { firstName: 'Agustín', lastName: 'M.', country: 'Chile', continent: 'Americas', age: 37, language: 'C' },
+  { firstName: 'Jing', lastName: 'X.', country: 'China', continent: 'Asia', age: 39, language: 'Ruby' },
+  { firstName: 'Laia', lastName: 'P.', country: 'Andorra', continent: 'Europe', age: 55, language: 'Ruby' },
+  { firstName: 'Oliver', lastName: 'Q.', country: 'Australia', continent: 'Oceania', age: 65, language: 'PHP' },
+];
+your function should return true as there is at least one developer from the required 5 geographic zones.
+
+Notes:
+    The input array and continent names will always be valid and formatted as in the list above for example 'Africa' will always start with upper-case 'A'. */
+
+
+function allContinents1(list) {
+  const arrayOfContinents = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  
+  const mapOfDevContinents = new Set(list.map(dev => dev.continent));
+  
+  return arrayOfContinents.length === mapOfDevContinents.size;
+}
+
+
+//OR using every and some methods combined:
+
+function allContinents2(list) {
+  return ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'].every(x => list.some(y => x==y.continent));
 }
