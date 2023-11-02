b25e755cc584cf81daeda631c6d552188440cf6e
Luka-DV <luka.guva@gmail.com>
2023-11-02 10:03:00 +0100

    add kata Replace With Alphabet Position (6)

--- main.js
+++ main.js
@@ -5339,6 +5339,27 @@ function spinWords(string){
                                           .join("");
                             } else return word;
                            });
-                           
+
   return reversedWordsArr.join(" ");
+}
+
+
+/* ++++++++++++6 kyu
+Replace With Alphabet Position
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+If anything in the text isn't a letter, ignore it and don't return it.
+"a" = 1, "b" = 2, etc.
+Example:
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string ) */
+
+
+function alphabetPosition(text) {
+  
+  const alhphabetString = "abcdefghijklmnopqrstuvwxyz";
+  
+  return text.toLowerCase().split("")
+    .filter(char => char.charCodeAt() >= 97 && char.charCodeAt() <= 122 )
+    .map((letter) => alhphabetString.indexOf(letter) + 1)
+    .join(" ");
 }
