61b727e65e9573626f1a89853d43fdcbda64b883
Luka-DV <luka.guva@gmail.com>
2023-11-11 15:49:39 +0100

    kata Simple Web Framework 1: Create a basic router (6)

--- main.js
+++ main.js
@@ -5742,3 +5742,56 @@ const snoopy1 = new Dog2("Beagle");
 
 const scoobydoo1= new Dog2("Great Dane");
 
+/*+++++++++++++6 kyu
+Simple Web Framework #1: Create a basic router
+In this Kata, you have to design a simple routing class for a web framework.
+The router should accept bindings for a given url, http method and an action.
+Then, when a request with a bound url and method comes in, it should return the result of the action.
+Example usage:
+var router = new Router;
+router.bind('/hello', 'GET', function(){ return 'hello world'; });
+router.runRequest('/hello', 'GET') // returns 'hello world';
+When asked for a route that doesn't exist, router should return:
+'Error 404: Not Found'
+The router should also handle modifying existing routes. See the example tests for more details. */
+
+//solution:
+
+const Router1 = function() {  
+  this.GET = {};
+  this.POST = {};
+  
+  this.bind = function(url, httpMethod, action) {
+    this[httpMethod][url] = action;
+  }
+  
+  this.runRequest = function(url, httpMethod) {
+    return this[httpMethod][url] ? this[httpMethod][url]() : 'Error 404: Not Found';
+  }
+}
+
+// solution for any httpMethod method:
+
+const Router2 = function() { 
+  
+  this.bind = function(url, httpMethod, action) {
+    if(!this[httpMethod]) {
+      this[httpMethod] = {};
+    }
+    this[httpMethod][url] = action;
+  }
+  
+  this.runRequest = function(url, httpMethod) {
+    if(this[httpMethod] && this[httpMethod][url]) {
+      return this[httpMethod][url]();
+    } else {
+      return 'Error 404: Not Found';
+    }
+  }
+}
+
+
+const router = new Router2;
+router.bind('/hello', 'GET', function(){ return 'hello world'; });
+
+console.log(router.runRequest('/hello', 'GET')) // returns 'hello world';
