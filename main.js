3e4a6d78dfe400c0ccbcd1806a7a4b65d50bee1d
Luka-DV <luka.guva@gmail.com>
2023-11-16 12:54:22 +0100

    add Count of positives / sum of negatives kata (8)

--- main.js
+++ main.js
@@ -5960,3 +5960,57 @@ function wave2(str){
 
 console.log(wave2("hello world"));
 
+
+/* ++++++++++++8 kyu
+Count of positives / sum of negatives
+Given an array of integers.
+Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers. 0 is neither positive nor negative.
+If the input is an empty array or is null, return an empty array.
+Example
+For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65] */
+
+function countPositivesSumNegatives(input) {
+  
+  if(!input) return [];
+  return input.reduce((acc, crr) => {
+    if(crr > 0) {
+      acc[0] = (acc[0] || 0) + 1;
+      acc[1] = acc[1] ?? 0;
+    } else if (crr < 0) {
+      acc[1] = (acc[1] || 0) + crr;
+      acc[0] = acc[0] ?? 0;
+    }
+    return acc;
+  },[])
+}
+ //OR:
+
+function countPositivesSumNegatives(input) {
+  
+  if(!input || input[0] === undefined) return [];
+  
+  let positiveNum = 0;
+  let negativeNum = 0;
+  
+  input.forEach(item => {
+    item > 0 ? positiveNum++ : item < 0 ? negativeNum += item : null;
+  })
+  
+  return [positiveNum, negativeNum];
+}
+
+//OR - best of both solutions:
+
+function countPositivesSumNegatives(input) {
+  if(!input || !input.length) return [];
+  
+  return input.reduce((acc, crr) => {
+    if(crr > 0) {
+      acc[0] = (acc[0] || 0) + 1;
+    } else if (crr < 0) {
+      acc[1] = (acc[1] || 0) + crr;
+    }
+    return acc;
+  },[0,0]);
+}
+
