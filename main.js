6d0a135119a4b4f3b82904d20f3edd6e7971131b
Luka-DV <luka.guva@gmail.com>
2023-12-13 11:45:28 +0100

    add Number of Decimal Digits

--- main.js
+++ main.js
@@ -6465,3 +6465,30 @@ function firstNonRepeatingLetter(s) {
   }
    return "";
 }
+
+
+/* ++++++++7 kyu
+Number of Decimal Digits
+Determine the total number of digits in the integer (n>=0) given as input to the function. For example, 9 is a single digit, 66 has 2 digits and 128685 has 6 digits. Be careful to avoid overflows/underflows.
+All inputs will be valid. */
+
+
+function digits(n) {
+  let numOfDigits = 0;
+  const numberToString = String(n);
+  
+  for(let digit of numberToString) {
+    numOfDigits++;
+  }
+  
+  return numOfDigits;
+}
+
+//or just:
+
+function digits(n) {
+  return String(n).length;
+}
+
+
+
