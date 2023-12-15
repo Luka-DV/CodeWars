36fe4b79e25ee80dbf5efa86bdae6c50b27ee4ab
Luka-DV <luka.guva@gmail.com>
2023-12-15 08:23:39 +0100

    add Greet Me kata (7)

--- main.js
+++ main.js
@@ -6550,3 +6550,19 @@ function towerBuilder(nFloors) {
   
   return towerArray;
 }
+
+/* ++++++++7 kyu
+Greet Me
+Write a method that takes one argument as name and then greets that name, capitalized and ends with an exclamation point.
+Example:
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!" */
+
+const greet = function(name) {
+  
+  const correctFormName = name[0].toUpperCase() + name.slice(1).toLowerCase();
+  return `Hello ${correctFormName}!`
+};
+
+
+
