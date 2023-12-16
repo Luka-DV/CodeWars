4fdc28a24752a04dc9cfbdad82c72c8b875b2952
Luka-DV <luka.guva@gmail.com>
2023-12-16 21:59:46 +0100

    add Even numbers in array kata (7)

--- main.js
+++ main.js
@@ -6564,5 +6564,40 @@ const greet = function(name) {
   return `Hello ${correctFormName}!`
 };
 
+/* ++++7 kyu
+Even numbers in an array
+Given an array of numbers, return a new array of length number containing the last even numbers from the original array (in the same order). The original array will be not empty and will contain at least "number" even numbers.
+([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8] */
 
+function evenNumbers(array, number) {
+  
+  let requiredNum = 0;
+  const evenNumsArray = [];
+  
+  for(let i = array.length -1; i >= 0; i--) {
+    if(array[i] % 2 === 0) {
+      evenNumsArray.push(array[i]);
+      requiredNum++;
+      if(requiredNum === number) {
+        return evenNumsArray.reverse();
+      }
+    }
+  }
+}
+
+//or:
+
+function evenNumbers(array, number) {
+  
+  const evenNumsArray = array.filter(num => num % 2 === 0);
+  
+  return evenNumsArray.slice(-number);
+}
+
+/* function evenNumbers(array, number) {
+  
+  return array.filter(num => num % 2 === 0)
+              .slice(-number);
+  
+} */
 
