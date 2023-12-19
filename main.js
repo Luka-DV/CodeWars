21c5acc719bcf31eedd189a17fd04f5e676581b2
Luka-DV <luka.guva@gmail.com>
2023-12-19 22:22:09 +0100

    add Re-open class kata

--- main.js
+++ main.js
@@ -6680,3 +6680,17 @@ class Cube2 {
 }
 
 
+
+/* ++++++++7 kyu
+Re-open class
+Ruby and Javascript let you re-open classes so you can add new functionalities to existing classes and objects.
+To demonstrate this, you'll have to add a new method (called my_new_method in Ruby or myNewMethod in JS / Coffeescript) into the String class that simply calls the upcase method (toUpperCase() in JS / Coffeescript), so that:
+returns
+"ABC" */
+
+String3.prototype.myNewMethod = function() {
+  return this.toUpperCase();
+}
+
+
+
