b122c6c8881bd602d0947332e3280761514b7650
Luka-DV <luka.guva@gmail.com>
2023-12-26 13:40:11 +0100

    add Tic-Tac-Toe Checker kata (5)

--- main.js
+++ main.js
@@ -6798,4 +6798,78 @@ function dataReverse(data) {
 }
 
 
+/* ++++5 kyu
+Tic-Tac-Toe Checker
+If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
 
+Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an "X", or 2 if it is an "O", like so:
+
+[[0, 0, 1],
+ [0, 1, 2],
+ [2, 1, 0]]
+
+We want our function to return:
+
+    -1 if the board is not yet finished AND no one has won yet (there are empty spots),
+    1 if "X" won,
+    2 if "O" won,
+    0 if it's a cat's game (i.e. a draw).
+
+You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe. */
+
+
+function isSolved(board) {
+  
+  let xWon = false;
+  let oWon = false;
+  let unfinished = false;
+  
+  for(let i = 0; i < 3; i++) { //horizontal and vertical checks
+      if(board[i].every(field => field === 1)) { //horizontal
+        xWon = true;
+        break;
+      } else if (board[i].every(field => field === 2)) {
+        oWon = true;
+        break;
+      } else if (board[0][i] === board[1][i] && board[0][i] === board[2][i]) { //vertical
+          if(board[0][i] === 1) {
+            xWon = true;
+            break;
+          } else if (board[0][i] === 2) {
+            oWon = true;
+            break;
+          }
+        }
+  }
+  
+  if(!xWon && !oWon) { //diagonal 1
+     if(board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
+      if(board[0][0] === 1) {
+        xWon = true;
+      } else if (board[0][0] === 2) {
+        oWon = true;
+      }
+    }
+  }
+ 
+  if(!xWon && !oWon) { //diagonal 2
+     if(board[0][2] === board[1][1] && board[0][2] === board[2][0]) {
+      if(board[0][2] === 1) {
+        xWon = true;
+      } else if (board[0][2] === 2) {
+        oWon = true;
+      }
+    }
+  }
+  
+  if(!xWon && !oWon) { //unfinished
+    for(let i = 0; i < 3; i++) {
+      if(board[i].some(field => field === 0)) {
+      unfinished = true;
+      break;
+      } 
+    }
+  }
+  
+  return xWon ? 1 : oWon ? 2 : unfinished ? -1 : 0;    
+}
