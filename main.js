facd48ab6cedb69f5053d38ec41e38eae3158d1d
Luka-DV <luka.guva@gmail.com>
2023-12-29 08:52:09 +0100

    add Array Helpers kata (6)

--- main.js
+++ main.js
@@ -7031,3 +7031,52 @@ const sortme2 = function( names ){
 
 //MDN: If omitted (the compare func.), the array elements are converted to strings, then sorted according to each character's Unicode code point value.
 
+
+/* +++++++++++6 kyu
+Array Helpers
+This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+Explanation:
+    square() must return a copy of the array, containing all values squared
+    cube() must return a copy of the array, containing all values cubed
+    average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+    sum() must return the sum of all array values
+    even() must return an array of all even numbers
+    odd() must return an array of all odd numbers
+Note: the original array must not be changed in any case! */
+
+Array1.prototype.square = function() {
+  return this.map(num => num**2);
+}
+
+Array1.prototype.cube = function() {
+  return this.map(num => num**3);
+}
+
+Array1.prototype.average = function() {
+  return this.reduce((acc, crr) => acc + crr, 0)/this.length;
+}
+
+Array1.prototype.sum = function() {
+   return this.reduce((acc, crr) => acc + crr, 0)
+}
+
+Array1.prototype.even = function() {
+  return this.filter(num => num % 2 === 0);
+}
+
+Array1.prototype.odd = function() {
+  return this.filter(num => num % 2 === 1);
+}
+
+//or, more effective:
+
+Object.assign(Array1.prototype, {
+  square() {return this.map(num => num**2);},
+  cube() {return this.map(num => num**3);},
+  sum() {return this.reduce((acc,crr) => acc + crr, 0);},
+  average() {return this.reduce((acc,crr) => acc + crr, 0) / this.length;},
+  even() {return this.filter(num => !(num % 2));},
+  odd() {return this.filter(num => num % 2);}
+});
+
+
