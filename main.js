7e383221b5275978b8e0d9b47b76360c82ea1f67
Luka-DV <luka.guva@gmail.com>
2023-12-30 21:13:56 +0100

    add Array Deep Count kata (6)

--- main.js
+++ main.js
@@ -7118,3 +7118,42 @@ function min2(arr, toReturn) {
 }
 
 
+/* ++++++++++6 kyu
+Array Deep Count
+You are given an array. Complete the function that returns the number of ALL elements within an array, including any nested arrays.
+Examples:
+[]                   -->  0
+[1, 2, 3]            -->  3
+["x", "y", ["z"]]    -->  4
+[1, 2, [3, 4, [5]]]  -->  7
+The input will always be an array. */
+
+
+function deepCount1(a, count = 0){
+
+  for(let element of a) {
+    count++
+    
+    if(typeof element === "object"){
+      count += deepCount(element);
+    }   
+  }
+  
+  return count;
+}
+
+//or:
+
+function deepCount2(a, count = a.length){
+  
+  for(let element of a) {
+    if(Array.isArray(element)) {
+      count += deepCount(element)
+    }
+  }
+  
+  return count;
+}
+
+
+
