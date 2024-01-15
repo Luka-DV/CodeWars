d6399fdf3ef3f83536d4c5a68f64d2fd353d1b6a
Luka-DV <luka.guva@gmail.com>
2024-01-15 19:56:05 +0100

    add Flatten kata (7)

--- main.js
+++ main.js
@@ -7633,3 +7633,20 @@ function findOutlier(integers){
   }
 }
 
+/* ++7 kyu
+Flatten
+Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+flatten([1,2,3]) // => [1,2,3]
+flatten([[1,2,3],["a","b","c"],[1,2,3]])  // => [1,2,3,"a","b","c",1,2,3]
+flatten([[[1,2,3]]]) // => [[1,2,3]] */
+
+function flatten(array){
+  return array.flat();
+}
+
+//CW:
+
+const flatten = function (lol){
+  return [].concat.apply([],lol);
+}
+
