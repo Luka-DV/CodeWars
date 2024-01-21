a8ab3a763b73af5c814f3daef002bf8f25789da2
Luka-DV <luka.guva@gmail.com>
2024-01-21 19:11:58 +0100

    add Get key/value pairs as arrays kata (7)

--- main.js
+++ main.js
@@ -7699,3 +7699,16 @@ function upArray(arr){
   return arr;
 }
 
+
+/* ++++7 kyu
+Get key/value pairs as arrays
+Complete the keysAndValues function so that it takes in an object and returns the keys and values as separate arrays.
+Example:
+keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]] */
+
+function keysAndValues(data){
+  const keysAndValuesArrays = [Object.keys(data), Object.values(data)];
+  return keysAndValuesArrays;
+}
+
+
