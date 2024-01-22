0b702f2cc6c03961d257dec3500b2d64a282051f
Luka-DV <luka.guva@gmail.com>
2024-01-22 20:36:41 +0100

    add Lottery Ticket kata (6)

--- main.js
+++ main.js
@@ -7767,3 +7767,33 @@ function reverse34(str){
   return reversedWordsArr.join(" ");
 }
 
+/* +++++6 kyu
+Lottery Ticket
+Given a lottery ticket (ticket), represented by an array of 2-value arrays, you must find out if you've won the jackpot.
+Example ticket:
+[ [ 'ABC', 65 ], [ 'HGR', 74 ], [ 'BYHT', 74 ] ]
+To do this, you must first count the 'mini-wins' on your ticket. Each subarray has both a string and a number within it. If the character code of any of the characters in the string matches the number, you get a mini win. Note you can only have one mini win per sub array.
+Once you have counted all of your mini wins, compare that number to the other input provided (win). If your total is more than or equal to (win), return 'Winner!'. Else return 'Loser!'.
+All inputs will be in the correct format. Strings on tickets are not always the same length. */
+
+function bingo(ticket, win){
+  
+  let winCounter = 0;
+  
+  for(let [string, num] of ticket) {
+    for(let i = 0; i < string.length; i++) {
+      if(string.charCodeAt(i) === num) {
+        winCounter++;
+        break;
+      }
+    }
+  }
+  
+  if(winCounter >= win) {
+    return "Winner!";
+  } else {
+    return "Loser!";
+  }
+}
+
+
