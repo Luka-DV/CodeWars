aaf9327afbf096ed6d637e186739883b22706351
Luka-DV <luka.guva@gmail.com>
2024-01-23 11:44:46 +0100

    add Largest Elements kata (7ž)

--- main.js
+++ main.js
@@ -7827,3 +7827,14 @@ Object.defineProperty(Array.prototype, 'numberOfOccurrences',{
     },0)
   }
 });
+
+
+/* 7 kyu
+Largest Elements
+Write a program that outputs the top n elements from a list.
+Example:
+largest(2, [7,6,5,4,3,2,1])
+// => [6,7] */
+
+
+
