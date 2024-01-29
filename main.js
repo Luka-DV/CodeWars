b417303388858726008c7ee6e05e6ab140c2d2dd
Luka-DV <luka.guva@gmail.com>
2024-01-29 20:04:20 +0100

    add Take a Ten Minutes Walk kata (6)

--- main.js
+++ main.js
@@ -8015,6 +8015,60 @@ function maxDiff(list) {
   return Math.max(...list) - Math.min(...list);
 };
 
+/* ++++++6 kyu
+Take a Ten Minutes Walk
+You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise. */
+
+
+
+function isValidWalk(walk) {
+  
+  if(walk.length !== 10) {
+    return false;
+  }
+  
+  let xAxis = 0;
+  let yAxis = 0;
+  
+  for(let direction of walk) {
+    if(direction === "n") {
+      yAxis++;
+    } else if(direction === "s") {
+      yAxis--;
+    } else if(direction === "e") {
+      xAxis++;
+    } else {
+      xAxis--;
+    }
+  };
+  
+  return xAxis === 0 && yAxis === 0;
+}
+
+//switch solution (CW):
+
+function isValidWalk(walk) {
+
+  const dt = walk.length;  
+  if(dt !== 10) return false;
+  
+  let dx = 0;
+  let dy = 0;
+  
+  for (let i = 0; i < dt; i++) {
+    switch (walk[i]) {
+      case 'n': dy--; break
+      case 's': dy++; break
+      case 'w': dx--; break
+      case 'e': dx++; break
+    }
+  };
+  
+  return dx === 0 && dy === 0;
+}
+
+
+
 
 
 
