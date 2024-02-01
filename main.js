ef0940c5187f9e374d29d902e9676e06a2ca9aea
Luka-DV <luka.guva@gmail.com>
2024-02-01 10:55:39 +0100

    add  Sum of angles kata (7)

--- main.js
+++ main.js
@@ -8147,3 +8147,8 @@ function dirReduc(arr){
   }, [])
 }
 
+
+/* ++++7 kyu
+Sum of angles
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2. */
+
