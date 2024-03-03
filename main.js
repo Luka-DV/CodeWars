f157c9b918d905132b53f75874e0284c29b782b4
Luka-DV <luka.guva@gmail.com>
2024-03-03 17:42:49 +0100

    add Form the minimum kata (7)

--- main.js
+++ main.js
@@ -8455,3 +8455,26 @@ const maxTriSum = numbers => {
   return a+b+c;
 }
 
+
+
+/* ++++7 kyu
+Form The Minimum
+Task
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+Notes:
+    Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+Input >> Output Examples
+  minValue ({1, 3, 1})  ==> return (13) */
+
+
+
+  function minValue(values){
+  
+    const uniqueValues = Array.from(new Set(values));
+    
+    uniqueValues.sort((a,b) => a - b );
+    
+    return Number(uniqueValues.join(""));
+  }
+
+  
