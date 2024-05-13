f7b0706298bfe510906afacc63044b4ca300da88
Luka-DV <luka.guva@gmail.com>
2024-05-13 11:33:55 +0200

    add Count characters in your string kata (6)

--- main.js
+++ main.js
@@ -8735,3 +8735,21 @@ function riders(stations) {
   return numOfRiders;
 }
 
+
+/* +++++6 kyu
+Count characters in your string
+The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+What if the string is empty? Then the result should be empty object literal, {}.
+ */
+
+function count(string) {
+  
+  const charCount = new Object();
+  
+  for(let letter of string) {
+    charCount[letter] = (charCount[letter] ?? 0) + 1;
+  }
+  
+  return charCount;
+}
+
