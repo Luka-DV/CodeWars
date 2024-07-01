f0fbb4098ae4f96771d181fcd3bc4ca2103931ec
Luka-DV <luka.guva@gmail.com>
2024-07-01 15:32:34 +0200

    add Vowel Count kata (7)

--- main.js
+++ main.js
@@ -9125,3 +9125,40 @@ function printFizzBuzz(n) {
 
 
 printFizzBuzz(50);
+
+/* 7 kyu
+Vowel Count
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces. */
+
+function getCount(str) {
+  let counter = 0;
+  const vowelsArr = ["a", "e", "i", "o", "u"];
+  
+  for(let char of str) {
+    if(vowelsArr.includes(char)) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+//or:
+
+function getCount(str) {
+  const vowelsArr = ["a", "e", "i", "o", "u"];
+  return str.split("")
+    .reduce((acc, crr) => {
+      if(vowelsArr.includes(crr)) {
+        acc++;
+      }
+    return acc;
+  }, 0);
+}
+
+
+
