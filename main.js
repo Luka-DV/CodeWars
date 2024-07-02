8fcdee62514bcec0c6c860d15361049f205859a9
Luka-DV <luka.guva@gmail.com>
2024-07-02 21:42:09 +0200

    add Square Every Digit kata (7)

--- main.js
+++ main.js
@@ -9185,7 +9185,7 @@ function solution(number){
   if(number < 0) return 0
   let counter = 0;
   const validNums = [3, 5]
-  
+
   for(let i = 0; i < number; i++) {
     for(let num of validNums) {
       if(i % num === 0) {
@@ -9198,3 +9198,21 @@ function solution(number){
   return counter;
 }
 
+
+/* 7 kyu
+Square Every Digit
+In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+Note: The function accepts an integer and returns an integer. */
+
+function squareDigits(num){
+  return Number(String(num) //or parseInt()
+    .split("")
+    .map(num => num**2)
+    .join("")
+    )
+}
