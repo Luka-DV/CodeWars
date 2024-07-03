5a6b7b50744509d59ba2fe30dfe04759a6afa329
Luka-DV <luka.guva@gmail.com>
2024-07-03 11:16:25 +0200

    add Highest and Lowest kata (7)

--- main.js
+++ main.js
@@ -9216,3 +9216,40 @@ function squareDigits(num){
     .join("")
     )
 }
+
+
+/* 7 kyu
+Highest and Lowest
+In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+Examples
+highAndLow("1 2 3 4 5");  // return "5 1"
+highAndLow("1 2 -3 4 5"); // return "5 -3"
+highAndLow("1 9 3 4 -5"); // return "9 -5"
+Notes
+    All numbers are valid Int32, no need to validate them.
+    There will always be at least one number in the input string.
+    Output string must be two numbers separated by a single space, and highest number is first. */
+
+
+function highAndLow(numbers){
+  const numArray = numbers.split(" ").map(Number);
+  let lowestNum = numArray[0];
+  let highestNum = numArray[0];
+  
+  for(let num of numArray) {
+    if(num < lowestNum) {
+      lowestNum = num;
+    } else if(num > highestNum) {
+      highestNum = num;
+    }
+  }
+  return `${highestNum} ${lowestNum}`
+}
+
+//or:
+
+function highAndLow(numbers){
+  const numArray = numbers.split(" ");
+
+  return `${Math.max(...numArray)} ${Math.min(...numArray)}`
+}
