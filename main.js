f69ad48e4abf8e8fc26a016824112b822900c603
Luka-DV <luka.guva@gmail.com>
2024-07-07 10:49:42 +0200

    add getLengthOfMissingArray kata (6)

--- main.js
+++ main.js
@@ -9353,3 +9353,64 @@ function titleCase(title, minorWords) {
         .join(" ");
 }
 
+/* 6 kyu
+Length of missing array
+You get an array of arrays.
+If you sort the arrays by their length, you will see, that their length-values are consecutive.
+But one array is missing!
+You have to write a method, that return the length of the missing array.
+Example:
+[[1, 2], [4, 5, 1, 1], [1], [5, 6, 7, 8, 9]] --> 3
+If the array of arrays is null/nil or empty, the method should return 0.
+When an array in the array is null or empty, the method should return 0 too!
+There will always be a missing element and its length will be always between the given arrays.  */
+
+function getLengthOfMissingArray(arrayOfArrays) {
+
+  if(!arrayOfArrays || arrayOfArrays.length === 0) { //check if null or empty
+    return 0;
+  }
+  
+  const arrayLengths = [];
+  for(let array of arrayOfArrays) {
+    if(!array || array.length === 0) { //check if array is null or empty
+      return 0
+    } else {
+      arrayLengths.push(array.length);
+    }
+  }
+  
+  arrayLengths.sort((a,b) => a-b);
+  
+  for(let i = 0; i < arrayLengths.length; i++) {
+    if(arrayLengths[i+1] !== arrayLengths[i] + 1) {
+      return arrayLengths[i] + 1;
+    }
+  }
+}
+
+
+//or:
+
+function getLengthOfMissingArray(arrayOfArrays) {
+
+  if(!arrayOfArrays || arrayOfArrays.length === 0) { //check if null or empty
+    return 0;
+  }
+  
+  const arrayLengths = arrayOfArrays.map( arr => {
+    if(!arr || arr.length === 0) {
+      return 0
+    } else {
+      return arr.length;
+    }})
+    .sort((a, b) => a-b);
+  
+  if(arrayLengths[0] === 0) return 0;
+  
+  for(let i = 0; i < arrayLengths.length; i++) {
+    if(arrayLengths[i+1] !== arrayLengths[i] + 1) {
+      return arrayLengths[i] + 1;
+    }
+  }
+}
