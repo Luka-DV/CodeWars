81b3d435b050205f796fa216661d220750329593
Luka-DV <luka.guva@gmail.com>
2024-07-19 20:40:32 +0200

    add Largest pari sum in array

--- main.js
+++ main.js
@@ -9824,3 +9824,28 @@ function tidyNumber(n){
   return true;
 }
 
+/* 7 kyu
+Largest pair sum in array
+Given a sequence of numbers, find the largest pair sum in the sequence.
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer. */
+
+function largestPairSum (numbers) {
+  
+  let largestNum = -Infinity;
+  let secondLargestNum = -Infinity;
+  
+  for(let i = 0; i < numbers.length; i++) {
+    if(numbers[i] > largestNum) {
+      secondLargestNum = largestNum;
+      largestNum = numbers[i];
+    } else if(numbers[i] > secondLargestNum) {
+      secondLargestNum = numbers[i];
+    }
+  }
+  
+  return largestNum + secondLargestNum;
+}
+
+
