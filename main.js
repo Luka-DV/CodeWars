f0d9bfa0227e69ab9678a5629582ec6527c4db17
Luka-DV <luka.guva@gmail.com>
2024-07-27 11:05:11 +0200

    add Testing 1-2-3 kata (7)

--- main.js
+++ main.js
@@ -9970,3 +9970,21 @@ Your recovered toString() methods should only return the stringified version of 
 //   return `[${this.join(",")}]`
 // };
 
+
+/* 7 kyu
+Testing 1-2-3
+Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+Examples: (Input --> Output)
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"] */
+
+const number = (array) => {
+  return array.map((element, ind) => `${ind + 1}: ${element}`);
+};
+
+
+
+
+
