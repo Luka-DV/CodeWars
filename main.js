64cf881e5b95bdbefeafceffb3b8821ff135df42
Luka-DV <luka.guva@gmail.com>
2024-07-29 21:25:53 +0200

    add Building blocks kata (7)

--- main.js
+++ main.js
@@ -10032,3 +10032,79 @@ class Dog extends Animal {
   }
 }
 
+
+
+/* 7 kyu
+Building blocks
+Write a class Block that creates a block (Duh..)
+Requirements
+The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+Define these methods:
+`getWidth()` return the width of the `Block`
+`getLength()` return the length of the `Block`
+`getHeight()` return the height of the `Block`
+`getVolume()` return the volume of the `Block`
+`getSurfaceArea()` return the surface area of the `Block` */
+
+class Block{
+
+  constructor([width, length , height]){
+    this.width = width;
+    this.length = length;
+    this.height = height;
+  }
+  
+  getWidth() {
+    return this.width;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getVolume() {
+    return this.height*this.width*this.length;
+  }
+
+  getSurfaceArea() {
+    return 2*(this.height*this.width + this.length*this.height + this.length*this.width)
+  }
+}
+
+//or:
+
+class Block{
+
+  constructor(data){
+  [ this.width ,
+    this.length ,
+    this.height ] = data;
+  }
+  
+  getWidth() {
+    return this.width;
+  }
+
+  getLength() {
+    return this.length;
+  }
+
+  getHeight() {
+    return this.height;
+  }
+
+  getVolume() {
+    const {width, length, height} = this;
+    return width*length*height;
+  }
+
+  getSurfaceArea() {
+    const {width, length, height} = this;
+    return 2*(height*width + length*height + length*width);
+  }
+}
+
