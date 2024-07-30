65d90d5b8f5bc6e53fea48cc531428b0c8132abd
Luka-DV <luka.guva@gmail.com>
2024-07-30 21:05:23 +0200

    add Number-like counter kata (7)

--- main.js
+++ main.js
@@ -10108,3 +10108,29 @@ class Block{
   }
 }
 
+/* 7 kyu
+Number-like counter
+You're going to implement a simple counter class. The counter will start at zero, and every time its incr method is called, it will increase by 1.
+There's one caveat: Your counter must act like a number and support arithmetic operations and comparisons.
+For example:
+var c = new Counter();
+c.incr(); // counter is now at 1
+c + 1; // 2
+c > 1; // false */
+
+
+class Counter {
+  constructor() {
+    this.counter = 0;
+  }
+  
+  incr() {
+    this.counter++;
+  }
+  
+  valueOf() {
+    return this.counter;
+  }
+}
+
+
