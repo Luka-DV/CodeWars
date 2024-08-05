d7ada984deb892f8b442a6f36173552b0eb46b60
Luka-DV <luka.guva@gmail.com>
2024-08-05 16:11:47 +0200

    add Chinese Zodiac and The Office III - Broken Photocopier katas (7 and 7)

--- main.js
+++ main.js
@@ -10269,3 +10269,34 @@ function sortByLength (array) {
   })
 }
 
+/* 7 kyu
+Chinese Zodiac
+The Chinese zodiac is a repeating cycle of 12 years, with each year being represented by an animal and its reputed attributes. The lunar calendar is divided into cycles of 60 years each, and each year has a combination of an animal and an element. There are 12 animals and 5 elements; the animals change each year, and the elements change every 2 years. The current cycle was initiated in the year of 1984 which was the year of the Wood Rat.
+Since the current calendar is Gregorian, I will only be using years from the epoch 1924. For simplicity I am counting the year as a whole year and not from January/February to the end of the year.
+##Task
+Given a year, return the element and animal that year represents ("Element Animal"). For example I'm born in 1998 so I'm an "Earth Tiger".
+animals (or $animals in Ruby) is a preloaded array containing the animals in order:
+['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig']
+elements (or $elements in Ruby) is a preloaded array containing the elements in order:
+['Wood', 'Fire', 'Earth', 'Metal', 'Water'] */
+
+function chineseZodiac(year){
+  const yearsPassed = year - 1924;
+  
+  return `${elements[Math.floor((yearsPassed/2)) % 5]} ${animals[yearsPassed % 12]}`;
+  
+}
+
+
+/* 7 kyu
+The Office III - Broken Photocopier
+The bloody photocopier is broken... Just as you were sneaking around the office to print off your favourite binary code!
+Instead of copying the original, it reverses it: '1' becomes '0' and vice versa.
+Given a string of binary, return the version the photocopier gives you as a string. */
+
+function broken(x){
+  return x.split("")
+    .map(num => num === "1" ? "0" : "1" )
+    .join("");
+}
+
