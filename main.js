c8702fb2cad292fc65480cee326aa5062c35e8d6
Luka-DV <luka.guva@gmail.com>
2024-08-06 16:02:40 +0200

    add Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer kata (6)

--- main.js
+++ main.js
@@ -10299,4 +10299,36 @@ function broken(x){
     .join("");
 }
 
+/* 6 kyu
+Coding Meetup #7 - Higher-Order Functions Series - Find the most senior developer
+Your task is to return a sequence which includes the developer who is the oldest. In case of a tie, include all same-age senior developers listed in the same order as they appeared in the original input array.
+For example, given the following input array:
+var list1 = [
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Odval', lastName: 'F.', country: 'Mongolia', continent: 'Asia', age: 38, language: 'Python' },
+  { firstName: 'Emilija', lastName: 'S.', country: 'Lithuania', continent: 'Europe', age: 19, language: 'Python' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+];
+your function should return the following array:
+[
+  { firstName: 'Gabriel', lastName: 'X.', country: 'Monaco', continent: 'Europe', age: 49, language: 'PHP' },
+  { firstName: 'Sou', lastName: 'B.', country: 'Japan', continent: 'Asia', age: 49, language: 'PHP' },
+]
+Notes:
+The input array will always be valid and formatted as in the example above and will never be empty. */
+
+function findSenior(list) {
+  let mostSeniorDevs = new Array();
+  
+  for(let dev of list) {
+    if( !mostSeniorDevs.length || dev.age > mostSeniorDevs[0].age) {
+      mostSeniorDevs = [dev];
+    } else if(dev.age === mostSeniorDevs[0].age) {
+      mostSeniorDevs.push(dev);
+    }
+  }
+  
+  return mostSeniorDevs;
+}
+
 
