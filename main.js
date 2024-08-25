0df134c9c12c391aad041e118ef70c74276b2df2
Luka-DV <luka.guva@gmail.com>
2024-08-25 19:03:09 +0200

    add Fun with ES6 Classes #4 - Cubes and Setters kata (7)

--- main.js
+++ main.js
@@ -10751,3 +10751,58 @@ class Cube extends Cuboid {
   }
 }
 
+/* 7 kyu
+Fun with ES6 Classes #4 - Cubes and Setters
+Define a class Cube whose constructor function takes exactly one parameter length and stores the value of the argument into this.length. You will then define both a getter and a setter for the surfaceArea and volume of the cube.
+No initial code will be given. You are free to use whatever syntax you like to complete this Kata but it is highly recommended that you use ES6 syntax and features to complete this Kata. */
+
+// ES6 class
+
+class Cube {
+  constructor(length) {
+    this.length = length;
+  }
+  
+  get surfaceArea() {
+    return 6*(this.length**2);
+  }
+  
+  get volume() {
+    return this.length**3;
+  }
+  
+  set surfaceArea(area) {
+    this.length = Math.sqrt(area/6);
+  }
+  
+  set volume(volume) {
+    this.length = Math.cbrt(volume);
+  }
+}
+
+// constructor function
+
+function Cube(length) {
+  
+  this.length = length;
+  
+  Object.defineProperties(this, {
+    surfaceArea: {
+      get() {
+        return 6*(Math.pow(this.length,2));
+     },
+      set(area) {
+         this.length = Math.sqrt(area/6);
+      }
+    },
+    volume: {
+      get() {
+       return Math.pow(this.length, 3);
+     },
+      set(volume) {
+        this.length = Math.cbrt(volume);
+      }
+    }
+    });
+}
+
