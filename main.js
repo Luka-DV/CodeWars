4ab32b979038fe7b034292945d44094757200102
Luka-DV <luka.guva@gmail.com>
2024-09-03 12:03:00 +0200

    add Split Strings kata (6)

--- main.js
+++ main.js
@@ -10957,3 +10957,17 @@ function twoSum(numbers, target) {
   }
 }
 
+/* 6 kyu
+Split Strings
+Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_'). */
+
+function solution(str){
+  const splitStringArray = [];
+ 
+ for(let i = 0; i < str.length; i += 2) {
+   splitStringArray.push(str[i] + (str[i + 1] ?? "_"));
+ }
+ 
+ return splitStringArray;
+}
+
