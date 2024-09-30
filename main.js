a723ce6b516667960bdc41b0f8720007981c70b4
Luka-DV <luka.guva@gmail.com>
2024-09-30 10:15:47 +0200

    add Stop gninnipS My sdroW! kata (6)

--- main.js
+++ main.js
@@ -11231,3 +11231,30 @@ function newCounter() {
 
 const addOneTest = newCounter();
 
+
+/* 6 kyu
+Stop gninnipS My sdroW!
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present. */
+
+function spinWords(string){
+  
+  return string.split(" ")
+    .map( word => {
+        if(word.length >= 5) {
+          return word.split("")
+            .reverse()
+            .join("");
+        }
+        return word;
+      })
+    .join(" ");
+}
+
+//or just:
+
+function spinWords(string){
+  return string.split(" ")
+    .map( word => word.length >= 5 ? word.split("").reverse().join("") : word)
+    .join(" ");
+}
+
